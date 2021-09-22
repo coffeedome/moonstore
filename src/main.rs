accfda893ff3ec74368c1843b176faeccdea406a
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-09-21 22:55:41 -0500

    working unit test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 mod record;
 
 fn main() {
     println!("Hello! What is your mood right now?...");
     let mut s = String::new();
-    std::io::stdin().read_line(&mut s).expect("Did you not enter a string?");
-    println!("Your mood right now is :{}",s);
-    match record::record_mood() {
+    std::io::stdin()
+        .read_line(&mut s)
+        .expect("Did you not enter a string?");
+    println!("Your mood right now is :{}", s);
+    match record::record_mood(
+        s,
+        "mood.db",
+        SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
+    ) {
         Ok(_) => println!("Well done!"),
-        Err(_) => println!("Error!")
+        Err(_) => println!("Error!"),
     }
 }
--- src/record.rs
+++ src/record.rs
@@ -1,8 +1,37 @@
-use std::{fs::{File}, io::Write};
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::{fs::OpenOptions, io::Write};
 
-pub fn record_mood() -> std::io::Result<()>{
-    println!("Mood recorded");
-    let mut file = File::create("mood.db")?;
-    file.write_all(b"Hello, Esteban!")?;
+pub fn record_mood(s: String, output_file: &str, t: Duration) -> std::io::Result<()> {
+    //create the file
+    File::create(output_file)?;
+
+    //append to the file
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(output_file)?;
+    let r = format!("{:?},{}", t, s);
+    file.write(r.as_bytes())?;
+    Ok(())
+}
+
+#[test]
+fn test_record_mood() -> std::io::Result<()> {
+    let time_stamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let mood = "Happy";
+
+    match record_mood(String::from(mood), "mood.test.db", time_stamp) {
+        Ok(_) => println!("Test file generated"),
+        Err(a) => println!("Failed to generate test file: {}", a),
+    }
+
+    //verify test file contents
+    let test_file = File::open("mood.test.db")?;
+    let mut buf_reader = BufReader::new(test_file);
+    let mut test_file_contents = String::new();
+    buf_reader.read_to_string(&mut test_file_contents)?;
+    assert_eq!(test_file_contents, format!("{:?},{}", time_stamp, "Happy"));
     Ok(())
 }
